16d2a3d39117c1e6db0fa9faf950a2e64537732f
Igor Karatayev <igor.cwer@gmail.com>
2019-10-14 19:21:44 +0300

    moved Symbols vsct section template here.

---
+++ VSCTEx.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using SystemEx;
+using VSIXEx.Templates;
+
+
+
+namespace VSIXEx
+{
+	public static class VSCTEx
+	{
+		public static string GenerateSymbols(this Assembly assembly)
+		{
+			return Template.TransformToText<VsctSymbols>(new
+			{
+				assembly = assembly
+			}.ToExpando());
+		}
+	}
+}
