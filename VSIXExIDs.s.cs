c8a1d7e0d0edb5e10646ae7634d23829e364248e
Igor Karatayev <igor.cwer@gmail.com>
2020-04-22 16:01:26 +0300

    update.

--- VSIXExIDs.s.cs
+++ VSIXExIDs.s.cs
@@ -1,13 +1,4 @@
-﻿namespace VSIXExCopy.s
-{
-	public static class VsGroupIDs
-	{
-		public const string IDM_VS_CTXT_CODEWIN = "VSIXExCopy.VsGroupIDs.IDM_VS_CTXT_CODEWIN, VSIXExCopy.VsGroupIDs, VSIXEx, Version=1.0.0.0, Culture=neutral, PublicKeyToken=a5c455218c41f680";
-		public const string IDM_VS_CTXT_EZDOCWINTAB = "VSIXExCopy.VsGroupIDs.IDM_VS_CTXT_EZDOCWINTAB, VSIXExCopy.VsGroupIDs, VSIXEx, Version=1.0.0.0, Culture=neutral, PublicKeyToken=a5c455218c41f680";
-	}
-}
-
-namespace VSIXEx.s
+﻿namespace VSIXEx.s
 {
 	public static class VsGroupIDs
 	{
