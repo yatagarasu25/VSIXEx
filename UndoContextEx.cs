210afbad668196b6328d5082bc9429062b2f1734
Igor Karatayev <igor.cwer@gmail.com>
2020-06-04 18:22:04 +0300

    added UndoContext scoped Open.

---
+++ UndoContextEx.cs
@@ -0,0 +1,39 @@
+﻿using EnvDTE;
+using System;
+
+namespace VSIXEx
+{
+	public static class UndoContextEx
+	{
+		private class ScopedUndoContext : IDisposable
+		{
+			UndoContext uc = null;
+
+			public ScopedUndoContext(UndoContext context, string Name, bool Strict = false)
+			{
+				Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+
+				if (!context.IsOpen)
+				{
+					uc = context;
+					uc.Open(Name, Strict);
+				}
+			}
+
+			public void Dispose()
+			{
+				Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+
+				if (uc != null)
+				{
+					uc.Close();
+				}
+			}
+		}
+
+		public static IDisposable Scoped(this UndoContext context, string Name, bool Strict = false)
+		{
+			return new ScopedUndoContext(context, Name, Strict);
+		}
+	}
+}
